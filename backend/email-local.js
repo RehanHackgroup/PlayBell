b16f63f4ac8fc4d194a63dfe3d2514e7b3d45cda
RehanHackShaikh <67001915+RehanHackgroup@users.noreply.github.com>
2025-12-09 23:20:55 +0530

    Update email-local.js

--- backend/email-local.js
+++ backend/email-local.js
@@ -2,5 +2,5 @@
 module.exports = {
   SMTP_USER: "connectwithplaybell@gmail.com",
   SMTP_PASS: "poojlkhvjlikkqby", // yahan apna Gmail App Password (spaces ke bina)
-  APP_BASE_URL: "http://localhost:3000"
+  APP_BASE_URL: "https://playbell.swiftbyte.dev"
 };
